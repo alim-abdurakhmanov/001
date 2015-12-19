1423191e0b0b2b8194779fcbac4e9277842e3e89
Seyda The <Seyda The>
2015-12-19 18:15:25 +0300

    change1

--- mod/registrationproc.php
+++ mod/registrationproc.php
@@ -11,7 +11,7 @@ if(!empty($_REQUEST['performer']) && $_REQUEST['performer']=="TRUE")
 		if(!empty($_REQUEST['phone'])){ $phone=$_REQUEST['phone']; $phone=mysql_escape_string($phone);}
 		if(!empty($_REQUEST['workid'])){ $workid=$_REQUEST['workid']; $workid=mysql_escape_string($workid);}
 		if(!empty($_REQUEST['region'])){ $region=$_REQUEST['region']; $region=mysql_escape_string($region);}
-		if(!empty($_REQUEST['pass'])){ $pass=$_REQUEST['pass']; $pass=mysql_escape_string($pass);}    
+		if(!empty($_REQUEST['pass'])){ $pass=$_REQUEST['pass']; $pass=mysql_escape_string($pass);$pass=sha1($pass);}    
 		$sql="INSERT INTO users (name, email, phone, password, workid, region, type, status, role)
 				VALUES ('$fio','$email','$phone','$pass','$workid','$region', 'perf', '0', 'user');";
 			if(ExecFromDB($sql)==TRUE)
@@ -33,7 +33,7 @@ if(!empty($_REQUEST['performer']) && $_REQUEST['performer']=="TRUE")
 		if(!empty($_REQUEST['workid'])){ $workid=$_REQUEST['workid']; $workid=mysql_escape_string($workid);}
 		if(!empty($_REQUEST['region'])){ $region=$_REQUEST['region']; $region=mysql_escape_string($region);}
 		if(!empty($_REQUEST['logo'])){ $logo=$_REQUEST['logo']; logoMake($logo);}
-		if(!empty($_REQUEST['pass'])){ $pass=$_REQUEST['pass']; $pass=mysql_escape_string($pass);} 
+		if(!empty($_REQUEST['pass'])){ $pass=$_REQUEST['pass']; $pass=mysql_escape_string($pass); $pass=sha1($pass);} 
 		$sql="INSERT INTO users (company, email, phone, password, region, workid, type, status, role)
 				VALUES ('$company','$email','$phone','$pass','$region','$workid', 'perf', '0', 'user');";
 			if(ExecFromDB($sql)==TRUE)
@@ -57,7 +57,7 @@ if((!empty($_REQUEST['customer']) && $_REQUEST['customer']=="TRUE"))
 	{
 		if(!empty($_REQUEST['fio'])){ $fio=$_REQUEST['fio']; $fio=mysql_escape_string($fio);}
 		if(!empty($_REQUEST['email'])){ $email=$_REQUEST['email']; $email=mysql_escape_string($email);}
-		if(!empty($_REQUEST['pass'])){ $pass=$_REQUEST['pass']; $pass=mysql_escape_string($pass);}
+		if(!empty($_REQUEST['pass'])){ $pass=$_REQUEST['pass']; $pass=mysql_escape_string($pass);$pass=sha1($pass);}
 		$sql="INSERT INTO users (fio, email, password, type, status, role)
 				VALUES ('$fio','$email','$pass', 'cust', '0', 'user');";
 			if(ExecFromDB($sql)==TRUE)
@@ -74,7 +74,7 @@ if((!empty($_REQUEST['customer']) && $_REQUEST['customer']=="TRUE"))
 	{
 		if(!empty($_REQUEST['company'])){ $company=$_REQUEST['company']; $company=mysql_escape_string($company);}
 		if(!empty($_REQUEST['email'])){ $email=$_REQUEST['email']; $email=mysql_escape_string($email);}
-		if(!empty($_REQUEST['pass'])){ $pass=$_REQUEST['pass']; $pass=mysql_escape_string($pass);}   
+		if(!empty($_REQUEST['pass'])){ $pass=$_REQUEST['pass']; $pass=mysql_escape_string($pass);$pass=sha1($pass);}   
 		$sql="INSERT INTO users (company, email, password,type, status, role)
 				VALUES ('$company','$email','$pass', 'cust', '0', 'user');";
 			if(ExecFromDB($sql)==TRUE)
